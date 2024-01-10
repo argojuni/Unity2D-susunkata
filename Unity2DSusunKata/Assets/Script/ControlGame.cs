29e45294f1eccf59e9bcd896d1ef34ed62484965
argojuni <juniargoprasaja05@gmail.com>
2024-01-10 15:03:38 +0700

    Function button left right

--- Unity2DSusunKata/Assets/Script/ControlGame.cs
+++ Unity2DSusunKata/Assets/Script/ControlGame.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 public class ControlGame : MonoBehaviour
@@ -35,9 +36,53 @@ public class ControlGame : MonoBehaviour
     public Transform parentKata;
 
     public float extraSpaceBoxKata;
+
+    [Header("Highligth target text")]
+
+    public int indexPosisiHightlight;
+
+    public GameObject[] cloneBoxKatas;
+
+
     void Start()//3
     {
         RandomImageSoal();    
+    }
+
+    public void ButtonLeftRightHighlight()
+    {
+        if(EventSystem.current.currentSelectedGameObject.name == "left")
+        {
+            indexPosisiHightlight -= 1;
+
+            if(indexPosisiHightlight < 0)
+            {
+                indexPosisiHightlight = cloneBoxKatas.Length - 1;//last element
+            }
+
+        }
+        else//right
+        {
+            indexPosisiHightlight += 1;
+
+            if (indexPosisiHightlight > cloneBoxKatas.Length - 1)
+            {
+                indexPosisiHightlight = 0;//first element
+            }
+        }
+
+        for (int i = 0; i < cloneBoxKatas.Length; i++)
+        {
+            if(i != indexPosisiHightlight)
+            {
+                cloneBoxKatas[i].transform.GetChild(1).gameObject.SetActive(false);//matikan semua
+            }
+            else
+            {
+                cloneBoxKatas[i].transform.GetChild(1).gameObject.SetActive(true);//nyalakan
+            }
+        }
+
     }
 
     void RandomImageSoal()//2
@@ -57,8 +102,6 @@ public class ControlGame : MonoBehaviour
         //Implementasi keterangan gambar
         splitStringImageSoal = stringImageSoals[indexRandomSprites[gameRound]].Split(' ');//dipotong dengan acuan spasi
 
-        RandomValueString(splitStringImageSoal);//random string
-
         lenghtPerText = new int[splitStringImageSoal.Length];
 
         for (int i = 0; i <lenghtPerText.Length; i++)
@@ -74,11 +117,15 @@ public class ControlGame : MonoBehaviour
             }
         }
 
+        cloneBoxKatas = new GameObject[splitStringImageSoal.Length];//create slot array
+
         //respon box
         for(int i=0; i<splitStringImageSoal.Length; i++)
         {
             GameObject cloneBoxKata = Instantiate(prefabBoxKata);//respawn
             cloneBoxKata.transform.SetParent(parentKata);//set parent
+
+            cloneBoxKatas[i] = cloneBoxKata;//fill array
 
             if(i == 0) //for change size x
             {
@@ -86,7 +133,9 @@ public class ControlGame : MonoBehaviour
 
                 textTerpanjang.text = splitStringImageSoal[indexTextTerpanjang];//get text
 
-                parentKata.GetComponent<GridLayoutGroup>().cellSize = new Vector2(textTerpanjang.preferredWidth + extraSpaceBoxKata,parentKata.GetComponent<GridLayoutGroup>().cellSize.y); 
+                parentKata.GetComponent<GridLayoutGroup>().cellSize = new Vector2(textTerpanjang.preferredWidth + extraSpaceBoxKata,parentKata.GetComponent<GridLayoutGroup>().cellSize.y);
+
+                cloneBoxKata.transform.GetChild(1).gameObject.SetActive(true); //Mengaktifkan highligh pertama
             }
 
             Text textCloneBoxKata = cloneBoxKata.transform.GetChild(0).GetComponent<Text>();//set text
