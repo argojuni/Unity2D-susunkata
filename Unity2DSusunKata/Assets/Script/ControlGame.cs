76baae4976e59eed460e4ae6ed9b1aa7e8a572c2
argojuni <juniargoprasaja05@gmail.com>
2024-01-11 19:26:30 +0700

    Movement Potongan Kata

--- Unity2DSusunKata/Assets/Script/ControlGame.cs
+++ Unity2DSusunKata/Assets/Script/ControlGame.cs
@@ -9,57 +9,114 @@ public class ControlGame : MonoBehaviour
     public int gameRound;
 
     [Header("Image Question")]
-    
-    public Image ImageSoal;
-    
+    public Image ImageSoal;    
     public Sprite[] spriteSoal;
-
     public int[] indexRandomSprites;
 
     [Tooltip("Jika ingin random tekan ini")]
-
     public bool isRandomSprite;
 
-    [Header("String keterangan gambar")]
-    
+    [Header("String keterangan gambar")]    
     public string[] stringImageSoals;
     public string[] splitStringImageSoal;
-
     public int[] lenghtPerText;
     public int indexTextTerpanjang;
 
 
     [Header("box kata")]
-
     public GameObject prefabBoxKata;
-
     public Transform parentKata;
-
     public float extraSpaceBoxKata;
 
     [Header("Highligth target text")]
-
     public int indexPosisiHightlight;
-
     public GameObject[] cloneBoxKatas;
 
     [Header("kata random")]
-
     public int[] indexRandomPotonganKata;
-
     public Text textPotonganKata;
-
     public int coundKata;
-
     public RectTransform rtPotonganKata;
-
     public float extraspacePotonganKata;
 
+    [Header("Move Potongan Kata")]
+    public GameObject potonganKata;
+    public float speedMovementPotonganKata;
+    public bool isStarMovePotonganKata;
+    Vector3 firstPositionPotonganKata;
+    public bool[] isKataBenar;
+    public GameObject coverBlockMovement;
+
     void Start()//3
-    {
+    {        
         RandomImageSoal();
 
         GeneratePotonganKata();
+
+        firstPositionPotonganKata = potonganKata.transform.position;//posisi awal
+
+        isKataBenar = new bool[splitStringImageSoal.Length];//create slot array
+    }
+
+    void Update()
+    {
+        MovementPotonganKata();    
+    }
+
+    public void ButtonPotonganKata()
+    {
+        if (isKataBenar[indexPosisiHightlight] == false)
+        {
+            isStarMovePotonganKata = true;
+
+            coverBlockMovement.SetActive(true);//aktifkan cover
+        }
+    }
+
+    void MovementPotonganKata()
+    {
+        if(isStarMovePotonganKata == true)
+        {
+            var step = speedMovementPotonganKata * Time.deltaTime;
+            potonganKata.transform.position = Vector3.MoveTowards(potonganKata.transform.position, cloneBoxKatas[indexPosisiHightlight].transform.position, step); //move object
+
+            if (Vector3.Distance(potonganKata.transform.position, cloneBoxKatas[indexPosisiHightlight].transform.position) < 0.001f)
+            {
+                Debug.Log("text sampai ke tujuan");
+
+                coverBlockMovement.SetActive(false);//matikan cover
+
+                potonganKata.transform.position = firstPositionPotonganKata;//reset position
+
+                if (textPotonganKata.text == splitStringImageSoal[indexPosisiHightlight]) //index posisi - count kata yang respon
+                {
+                    Debug.Log(textPotonganKata.text + " ? " + splitStringImageSoal[indexPosisiHightlight]);
+                    Debug.Log("Jawaban Benar");
+
+                    isKataBenar[indexPosisiHightlight] = true;//merubah bool atau kondisi box text
+
+                    cloneBoxKatas[indexPosisiHightlight].transform.GetChild(0).GetComponent<Text>().text = textPotonganKata.text;//change text dengan jawaban
+
+                    if (coundKata<splitStringImageSoal.Length - 1)
+                    {
+                        coundKata += 1;//increase karena benar
+                        textPotonganKata.text = splitStringImageSoal[indexRandomPotonganKata[coundKata]];//update text kata
+                        rtPotonganKata.sizeDelta = new Vector2(textPotonganKata.preferredWidth + extraspacePotonganKata, rtPotonganKata.sizeDelta.y);//resize box
+                    }
+                    else
+                    {
+                        Debug.Log("Generate Level Beikutnya");
+                    }                   
+                }
+                else
+                {
+                    Debug.Log("Jawaban Salah");
+                }
+                isStarMovePotonganKata = false;
+            }
+        }
+
+        
     }
 
     void GeneratePotonganKata()
